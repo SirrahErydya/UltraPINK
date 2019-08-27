6f886be41e3211d33173d0f1586bd18bec2c736f
Fenja Kollasch <kollasch@uni-bremen.de>
2019-08-27 15:35:36 +0200

    Toolbox layout

--- som/static/som/view_functions.js
+++ som/static/som/view_functions.js
@@ -2,9 +2,10 @@ function collapse(id, container_id) {
     button = document.getElementById(id);
     container = document.getElementById(container_id);
     button.classList.toggle('active');
-    if(container.style.display === 'block') {
+    display_style = container.style.display
+    if(display_style === 'grid') {
         container.style.display = 'none';
     } else {
-        container.style.display = 'block';
+        container.style.display = 'grid';
     }
 }
